abb8761c0bbf459e0cf0358fb387ea1b72b56c81
Jon Schwartz <jonschwartz@Jons-MacBook-Pro.local>
2025-08-14 14:58:40 -0400

    Some table style updates

--- src/app/components/AdvocateTable.tsx
+++ src/app/components/AdvocateTable.tsx
@@ -11,13 +11,13 @@ export function AdvocateTable({ advocates, loading }: { advocates: Advocate[]; l
     <table className="w-full font-sans bg-white text-left">
       <thead>
         <tr className="bg-gray-100">
-          <th className="px-4 py-2 font-semibold text-gray-700 border-b">First Name</th>
-          <th className="px-4 py-2 font-semibold text-gray-700 border-b">Last Name</th>
-          <th className="px-4 py-2 font-semibold text-gray-700 border-b">City</th>
-          <th className="px-4 py-2 font-semibold text-gray-700 border-b">Degree</th>
-          <th className="px-4 py-2 font-semibold text-gray-700 border-b">Specialties</th>
-          <th className="px-4 py-2 font-semibold text-gray-700 border-b">Years of Experience</th>
-          <th className="px-4 py-2 font-semibold text-gray-700 border-b">Phone Number</th>
+          <th className="py-2 font-semibold text-gray-700 border-b">First Name</th>
+          <th className="py-2 font-semibold text-gray-700 border-b">Last Name</th>
+          <th className="py-2 font-semibold text-gray-700 border-b">City</th>
+          <th className="py-2 font-semibold text-gray-700 border-b">Degree</th>
+          <th className="py-2 font-semibold text-gray-700 border-b">Specialties</th>
+          <th className="py-2 font-semibold text-gray-700 border-b">Years of Experience</th>
+          <th className="py-2 font-semibold text-gray-700 border-b">Phone Number</th>
         </tr>
       </thead>
       <tbody>
