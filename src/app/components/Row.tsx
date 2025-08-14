a4cd6bd8a330d3e2e6aec38ecb715ad2c13026fc
Jon Schwartz <jonschwartz@Jons-MacBook-Pro.local>
2025-08-14 14:32:22 -0400

    Update Row styles

--- src/app/components/Row.tsx
+++ src/app/components/Row.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Advocate } from "../types";
 
-export function AdvocateRow({ advocate }: { advocate: Advocate }) {
+export function Row({ advocate }: { advocate: Advocate }) {
   return (
-    <tr key={advocate.id}>
+    <tr
+      key={advocate.id}
+      style={{
+        backgroundColor: advocate.id % 2 === 0 ? "#f9f9f9" : "#ffffff",
+      }}
+    >
       <td>{advocate.firstName}</td>
       <td>{advocate.lastName}</td>
       <td>{advocate.city}</td>
       <td>{advocate.degree}</td>
       <td>
-        {advocate.specialties.map((s, idx) => (
-          <div key={idx}>{s}</div>
+        {advocate.specialties.map((s, index) => (
+          <div key={index}>{s}</div>
         ))}
       </td>
       <td>{advocate.yearsOfExperience}</td>
